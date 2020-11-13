787ad8c502ee5cc3eb770dd94c3181c2839bec78
Ziberian <emref99@hotmail.com>
2020-11-12 20:08:07 -0500

    doing tutorial, added collectible

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public int maxHealth = 5;
+    
+    //get is read only, set is write only
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    public float speed = 5.0f;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    //Unity executes this when the game starts
+    void Start()
+    {
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    //public because the health pick up needs to access it
+    public void ChangeHealth(int amount)
+    {
+        //ensures health is never above max, or less than 0
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
